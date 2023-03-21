5d606d3a9991dd38f1895f18043a52ea77506074
Joseph Catrambone <jo.jcat@gmail.com>
2023-03-20 22:34:05 -0700

    Randomize our input state because I forget to do that every time.

--- main/main_game.gd
+++ main/main_game.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
 	StoryManager.switch_scene("start")
 	#$Timer.start()
 	#await $Timer.timeout
